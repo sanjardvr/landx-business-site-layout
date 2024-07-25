a81052521a72838308ae7f67086a19840d31a3f5
Sanjar <lazabdufattahovsanjar10@gmail.com>
2024-07-25 10:33:07 +0500

    first"

---
+++ web/2021/front-end/project/js/jsProject.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+	$('.sixth-part_slider').slick({
+		arrows:false,
+		dots:true,
+		adaptiveHeight:true,
+		slidesToShow:3,
+		speed:500,
+		slidesToScroll:3,
+		infinite:true,
+		autoplay:true,
+		autoplaySpeed:2000,
+		pauseOnHover:false,
+	})
+})
